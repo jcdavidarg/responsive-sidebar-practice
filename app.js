d4f3cba56535fb2d8e5571afd534fafb310a2fe0
jcdavidarg <jcdavidarg@gmail.com>
2024-10-30 01:44:36 -0300

    close open sidebar

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+const toggleButton = document.querySelector("#toggle-btn");
+const sidebar = document.querySelector("#sidebar");
+
+const toggleSideBar = () => {
+    sidebar.classList.toggle("close");
+    toggleButton.classList.toggle("rotate");
+}
+
 const toggleSubMenu = (button) => {
     button.nextElementSibling.classList.toggle("show");
     button.classList.toggle("rotate");
